c37c043b49c053567cc1696eefc735c7c356fab7
n42 <n42@e5242d76-9126-0410-bb86-e0b726a7b37f>
2004-04-28 21:54:23 +0000

    wtf is this?
    git-svn-id: file:///tmp/svn/trunk/jhack@7 e5242d76-9126-0410-bb86-e0b726a7b37f

---
+++ src/net/starmen/pkhack/eb/PSIAnimEditor.java
@@ -0,0 +1,76 @@
+/*
+ * Created on Apr 28, 2004
+ */
+package net.starmen.pkhack.eb;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import net.starmen.pkhack.Rom;
+import net.starmen.pkhack.XMLPreferences;
+
+public class PSIAnimEditor extends EbHackModule implements ActionListener
+{
+    public PSIAnimEditor(Rom rom, XMLPreferences prefs)
+    {
+        super(rom, prefs);
+    }
+        public String getVersion()
+    {
+        return "0.1a";
+    }
+
+    public String getDescription()
+    {
+        return "PSI Animation Editor";
+    }
+
+    public String getCredits()
+    {
+        return "Written by n42";
+    }
+
+    public static class PSIAnim
+    {
+        private EbHackModule hm;
+        private boolean isInited = false;
+        private int num, arngPointer, dataPointer;
+        
+        // Base address for the tiles
+        public static final int baseAddr = 0xC0200;
+        
+        // Base address for the arrangement
+        public static final int arngPointers = 0xCF78F;
+        
+        // Base address for the PSI data
+        public static final int dataPointers = 0xCC2E19;
+        
+        
+        public PSIAnim(int i, EbHackModule hm)
+        {
+            this.hm = hm;
+            this.num = i;
+
+            arngPointer = toRegPointer(hm.rom.readMulti(arngPointer + (i * 4), 4));
+            dataPointer = toRegPointer(hm.rom.readMulti(arngPointer + (i * 4), 4));
+        }
+        public boolean readInfo()
+        {
+            if (isInited) return true;
+            
+            isInited = true;
+			return true;
+        }
+    }
+    
+    protected void init()
+    {
+    }
+    public void hide()
+    {
+        mainWindow.setVisible(false);
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+    }
+}
