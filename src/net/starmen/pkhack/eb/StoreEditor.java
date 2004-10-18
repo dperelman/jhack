dc29b8682eb04fc980adf55f182c07ab07d87890
anyoneeb <anyoneeb@e5242d76-9126-0410-bb86-e0b726a7b37f>
2004-10-18 00:17:08 +0000

    Store Editor: first store listed as null
    git-svn-id: file:///tmp/svn/trunk/jhack@123 e5242d76-9126-0410-bb86-e0b726a7b37f

--- src/net/starmen/pkhack/eb/StoreEditor.java
+++ src/net/starmen/pkhack/eb/StoreEditor.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -16,8 +17,8 @@ import net.starmen.pkhack.AbstractRom;
 import net.starmen.pkhack.XMLPreferences;
 
 /**
- * Editor for what items are sold in what stores. Requires ItemEditor because
- * it shows the item names and allows editing of their costs.
+ * Editor for what items are sold in what stores. Requires ItemEditor because it
+ * shows the item names and allows editing of their costs.
  * 
  * @author AnyoneEB
  * @see ItemEditor
@@ -28,7 +29,8 @@ public class StoreEditor extends EbHackModule implements ActionListener
      * @param rom
      * @param prefs
      */
-    public StoreEditor(AbstractRom rom, XMLPreferences prefs) {
+    public StoreEditor(AbstractRom rom, XMLPreferences prefs)
+    {
         super(rom, prefs);
     }
     private JComboBox selector;
@@ -40,7 +42,7 @@ public class StoreEditor extends EbHackModule implements ActionListener
      * @see #stores
      * @see #storeNames
      */
-    public static final int NUM_STORES = 65;
+    public static final int NUM_STORES = 66;
     /**
      * Array of all the {@link Store}'s in the game.
      * 
@@ -66,7 +68,7 @@ public class StoreEditor extends EbHackModule implements ActionListener
         if (!storeNamesInited)
         {
             storeNamesInited = true;
-            readArray(DEFAULT_BASE_DIR,"storeNames.txt", false, storeNames);
+            readArray(DEFAULT_BASE_DIR, "storeNames.txt", false, storeNames);
         }
     }
 
@@ -79,8 +81,7 @@ public class StoreEditor extends EbHackModule implements ActionListener
 
         initStoreNames();
         mainWindow.getContentPane().add(
-            HackModule.getLabeledComponent(
-                "Store: ",
+            HackModule.getLabeledComponent("Store: ",
                 selector = createJComboBoxFromArray(storeNames, false)),
             BorderLayout.NORTH);
         selector.setActionCommand("storeSelector");
@@ -91,11 +92,8 @@ public class StoreEditor extends EbHackModule implements ActionListener
         entry.add(pairComponents(new JLabel("Item"), new JLabel("Cost"), true));
         for (int i = 0; i < item.length; i++)
         {
-            entry.add(
-                pairComponents(
-                    item[i] = ItemEditor.createItemComboBox(this,this),
-                    money[i] = createSizedJTextField(5, true),
-                    true));
+            entry.add(pairComponents(item[i] = ItemEditor.createItemComboBox(
+                this, this), money[i] = createSizedJTextField(5, true), true));
             item[i].setActionCommand("storeItemList" + i);
             item[i].addActionListener(this);
         }
@@ -158,6 +156,7 @@ public class StoreEditor extends EbHackModule implements ActionListener
             stores[i] = new Store(i, rom);
         }
     }
+
     private void readFromRom()
     {
         readFromRom(rom);
@@ -171,20 +170,27 @@ public class StoreEditor extends EbHackModule implements ActionListener
         {
             item[j].setSelectedIndex(stores[i].getItem(j));
             item[j].repaint();
-            money[j].setText(
-                Integer.toString(
-                    ItemEditor.items[item[j].getSelectedIndex()].cost));
+            money[j].setText(Integer.toString(ItemEditor.items[item[j]
+                .getSelectedIndex()].cost));
         }
     }
+
     private void saveInfo(int i)
     {
         if (i < 0)
             return;
+        else if (i == 0)
+        {
+            JOptionPane.showMessageDialog(mainWindow,
+                "Writing to store #0 is not allowed.", "Write Failed",
+                JOptionPane.WARNING_MESSAGE);
+            return;
+        }
         for (int j = 0; j < item.length; j++)
         {
             stores[i].setItem(j, item[j].getSelectedIndex());
-            ItemEditor.items[item[j].getSelectedIndex()].cost =
-                Integer.parseInt(money[j].getText());
+            ItemEditor.items[item[j].getSelectedIndex()].cost = Integer
+                .parseInt(money[j].getText());
             ItemEditor.items[item[j].getSelectedIndex()].writeInfo();
         }
         stores[i].writeInfo();
@@ -198,9 +204,8 @@ public class StoreEditor extends EbHackModule implements ActionListener
             if (item[j].getSelectedIndex() < 0)
                 return;
             //last char
-            money[j].setText(
-                Integer.toString(
-                    ItemEditor.items[item[j].getSelectedIndex()].cost));
+            money[j].setText(Integer.toString(ItemEditor.items[item[j]
+                .getSelectedIndex()].cost));
         }
         else if (ae.getActionCommand().equals(selector.getActionCommand()))
         {
@@ -232,6 +237,7 @@ public class StoreEditor extends EbHackModule implements ActionListener
         private AbstractRom rom;
         private int address, num;
         private int[] items = new int[7];
+
         /**
          * Reads the information from the ROM on the given store number.
          * 
@@ -239,9 +245,9 @@ public class StoreEditor extends EbHackModule implements ActionListener
          */
         public Store(int num, AbstractRom rom)
         {
-            this.rom =rom;
+            this.rom = rom;
             this.num = num;
-            this.address = 0x1578b9 + (num * 7);
+            this.address = 0x1578b2 + (num * 7);
 
             rom.seek(this.address);
             for (int i = 0; i < items.length; i++)
@@ -263,8 +269,8 @@ public class StoreEditor extends EbHackModule implements ActionListener
         }
 
         /**
-         * Gets the item number of the <code>i</code> 'th item in this
-         * store's list.
+         * Gets the item number of the <code>i</code> 'th item in this store's
+         * list.
          * 
          * @param i Place in list to get item of.
          * @return Item number of item at <code>i</code>.
@@ -273,9 +279,10 @@ public class StoreEditor extends EbHackModule implements ActionListener
         {
             return items[i];
         }
+
         /**
-         * Sets the item number of the <code>i</code> 'th item in this
-         * store's list.
+         * Sets the item number of the <code>i</code> 'th item in this store's
+         * list.
          * 
          * @param i Place in list to set item of.
          * @param in Item number of item to set at <code>i</code>.
