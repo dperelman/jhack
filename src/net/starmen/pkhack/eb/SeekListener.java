b47e062e8f75f3895c9f89b5bcdfa8268a4463cd
mrtenda <mrtenda@e5242d76-9126-0410-bb86-e0b726a7b37f>
2004-11-26 03:17:18 +0000

    a listener for modules that lets them to seek from the MapEditor
    git-svn-id: file:///tmp/svn/trunk/jhack@152 e5242d76-9126-0410-bb86-e0b726a7b37f

---
+++ src/net/starmen/pkhack/eb/SeekListener.java
@@ -0,0 +1,10 @@
+package net.starmen.pkhack.eb;
+
+/**
+ * @author Mr. Tenda
+ *
+ * TODO Write javadoc for this interface
+ */
+public interface SeekListener {
+	void returnSeek(int x, int y, int tileX, int tileY);
+}
