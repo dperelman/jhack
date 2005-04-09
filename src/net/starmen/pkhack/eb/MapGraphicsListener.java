b1533bda8c69125a872321865309db484cca323a
mrtenda <mrtenda@e5242d76-9126-0410-bb86-e0b726a7b37f>
2005-04-09 20:48:07 +0000

    An interface to be used with the MapEditor.MapGraphics class Alerts of important events that occur within MapGraphics
    git-svn-id: file:///tmp/svn/trunk/jhack@333 e5242d76-9126-0410-bb86-e0b726a7b37f

---
+++ src/net/starmen/pkhack/eb/MapGraphicsListener.java
@@ -0,0 +1,11 @@
+package net.starmen.pkhack.eb;
+
+/**
+ * @author Mr. Tenda
+ *
+ * TODO Write javadoc for this interface
+ */
+public interface MapGraphicsListener {
+	void changedMode(int newMode, int oldMode);
+	void changedSector(boolean knowsSector, int sectorX, int sectorY);
+}
