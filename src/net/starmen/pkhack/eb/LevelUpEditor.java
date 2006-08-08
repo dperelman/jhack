e5b6627df77306bcb440ab0069025564aac2a751
anyoneeb <anyoneeb@e5242d76-9126-0410-bb86-e0b726a7b37f>
2006-08-08 01:25:08 +0000

    # Level-Up Experience Editor v0.3: Added import/export as .csv
    git-svn-id: file:///tmp/svn/trunk/jhack@510 e5242d76-9126-0410-bb86-e0b726a7b37f

--- src/net/starmen/pkhack/eb/LevelUpEditor.java
+++ src/net/starmen/pkhack/eb/LevelUpEditor.java
@@ -3,19 +3,24 @@ package net.starmen.pkhack.eb;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
-import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import net.starmen.pkhack.AbstractRom;
+import net.starmen.pkhack.HackModule;
 import net.starmen.pkhack.XMLPreferences;
 
 /**
@@ -31,22 +36,32 @@ public class LevelUpEditor extends EbHackModule implements ActionListener
      * @param rom
      * @param prefs
      */
-    public LevelUpEditor(AbstractRom rom, XMLPreferences prefs) {
+    public LevelUpEditor(AbstractRom rom, XMLPreferences prefs)
+    {
         super(rom, prefs);
     }
     private JComboBox charSelector, levelSelector;
     private JTextField exp;
-    private JButton dump;
     private boolean initing = true;
 
     protected void init()
     {
         mainWindow = createBaseWindow(this);
         mainWindow.setTitle(this.getDescription());
-        //mainWindow.setSize(250, 150);
+        // mainWindow.setSize(250, 150);
         mainWindow.setResizable(false);
 
-        //make a JComboBox to select entry, and a JTextField to edit it
+        JMenuBar mb = new JMenuBar();
+        JMenu file = new JMenu("File");
+        file.setMnemonic(KeyEvent.VK_F);
+        file.add(HackModule.createJMenuItem("Import From .csv", 'i', null,
+            "import", this));
+        file.add(HackModule.createJMenuItem("Export As .csv", 'e', null,
+            "export", this));
+        mb.add(file);
+        mainWindow.setJMenuBar(mb);
+
+        // make a JComboBox to select entry, and a JTextField to edit it
         JPanel entry = new JPanel();
         entry.setLayout(new BoxLayout(entry, BoxLayout.Y_AXIS));
 
@@ -71,11 +86,6 @@ public class LevelUpEditor extends EbHackModule implements ActionListener
         exp = createSizedJTextField(10, true);
         entry.add(getLabeledComponent("Experience points:", exp));
 
-        dump = new JButton("Dump stuff");
-        dump.setActionCommand("dump");
-        dump.addActionListener(this);
-        entry.add(dump);
-        
         mainWindow.getContentPane().add(entry, BorderLayout.CENTER);
         mainWindow.pack();
     }
@@ -85,7 +95,7 @@ public class LevelUpEditor extends EbHackModule implements ActionListener
      */
     public String getVersion()
     {
-        return "0.2";
+        return "0.3";
     }
 
     /**
@@ -125,33 +135,34 @@ public class LevelUpEditor extends EbHackModule implements ActionListener
     }
 
     /**
-     * Reads from the ROM the experience needed for <code>chr</code> to get
-     * to level <code>lvl</code>.
+     * Reads from the ROM the experience needed for <code>chr</code> to get to
+     * level <code>lvl</code>.
      * 
-     * @param chr Character to get exp needed of. 0 = Ness, 1 = Paula, 2 =
-     *            Jeff, 3 = Poo
+     * @param chr Character to get exp needed of. 0 = Ness, 1 = Paula, 2 = Jeff,
+     *            3 = Poo
      * @param lvl Level to get exp needed for. (2-99)
      * @return Exp needed by character <code>chr</code> to get to level
      *         <code>lvl</code>.
      */
     public static int readExp(int chr, int lvl, AbstractRom rom)
     {
-        //each entry 4 bytes, 99 entries/character
+        // each entry 4 bytes, 99 entries/character
         return rom.readMulti(0x159151 + (((lvl + (chr * 100)) - 2) * 4), 4);
     }
 
     /**
      * Writes to the ROM a new experience needed value. Takes <code>exp</code>
-     * as the experience needed for <code>chr</code> to get to level <code>lvl</code>.
+     * as the experience needed for <code>chr</code> to get to level
+     * <code>lvl</code>.
      * 
-     * @param chr Character to set exp needed of. 0 = Ness, 1 = Paula, 2 =
-     *            Jeff, 3 = Poo
+     * @param chr Character to set exp needed of. 0 = Ness, 1 = Paula, 2 = Jeff,
+     *            3 = Poo
      * @param lvl Level to set exp needed for. (2-99)
      * @param exp Value to set as the new exp needed.
      */
     public static void writeExp(int chr, int lvl, int exp, AbstractRom rom)
     {
-        //each entry 4 bytes, 99 entries/character
+        // each entry 4 bytes, 99 entries/character
         rom.write(0x159151 + (((lvl + (chr * 100)) - 2) * 4), exp, 4);
     }
 
@@ -175,61 +186,123 @@ public class LevelUpEditor extends EbHackModule implements ActionListener
         {
             showInfo(charSelector.getSelectedIndex(), (levelSelector
                 .getSelectedIndex() + 2));
-            //level counting starts at 2, index counting starts at 0
+            // level counting starts at 2, index counting starts at 0
         }
         else if (ae.getActionCommand().equals("apply"))
         {
             saveInfo(charSelector.getSelectedIndex(), (levelSelector
                 .getSelectedIndex() + 2));
-            //level counting starts at 2, index counting starts at 0
+            // level counting starts at 2, index counting starts at 0
         }
         else if (ae.getActionCommand().equals("close"))
         {
             hide();
         }
-        else if (ae.getActionCommand().equals("dump"))
+        else if (ae.getActionCommand().equals("export"))
         {
-        	dump();
+            exportExp(rom);
+        }
+        else if (ae.getActionCommand().equals("import"))
+        {
+            importExp(rom);
+            showInfo(charSelector.getSelectedIndex(), (levelSelector
+                .getSelectedIndex() + 2));
         }
     }
 
-    public void dump()
+    private static final String EXPORT_FIRST_LINE = "Level,Ness,Paula,Jeff,Poo";
+
+    public static void exportExp(AbstractRom rom)
     {
-    	int c;
-    	String[] dump = new String[4*98];
-    	for(int i = 0; i < 4*98; i++)
-    	{
-    		c = i/98;
-    		dump[i] = (c > 1 ? c == 3 ? "Poo" : "Jeff" : c==1 ? "Paula" : "Ness")
-				+ " " + (i%98 + 2 < 10 ? "0" : "") +
-				(i%98 + 2) + " " + (readExp(c, i%98 + 2, rom));
-    	}
-    	File file = getFile(true, "txt", "Text file");
-        String output = new String();
-        String[] in = dump;
-        boolean hexNum = false;
-        for (int i = 0; i < in.length; i++)
-        {
-            output += (in[i] + "\n");
-        }
+        exportExp(rom, getFile(true, "csv", "Comma Separated Values"));
+    }
+
+    public static void exportExp(AbstractRom rom, File f)
+    {
+        if (f == null)
+            return;
 
         try
         {
-            FileWriter out = new FileWriter(file);
-            out.write(output);
+            FileWriter out = new FileWriter(f);
+            out.write(EXPORT_FIRST_LINE);
+            for (int lvl = 2; lvl <= 99; lvl++)
+            {
+                out.write("\n" + lvl);
+                for (int chr = 0; chr < 4; chr++)
+                {
+                    out.write("," + readExp(chr, lvl, rom));
+                }
+            }
             out.close();
+            System.out.println(".CSV export successful.");
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("Error: File not saved: File not found.");
+            JOptionPane.showMessageDialog(null,
+                "Error: File not saved: File not found.", "Error",
+                JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
             return;
         }
         catch (IOException e)
         {
-            System.out.println("Error: File not saved: Could write file.");
+            JOptionPane.showMessageDialog(null,
+                "Error: File not saved: Could write file.", "Error",
+                JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
             return;
         }
-   }
+    }
+
+    public static void importExp(AbstractRom rom)
+    {
+        importExp(rom, getFile(false, "csv", "Comma Separated Values"));
+    }
+
+    public static void importExp(AbstractRom rom, File f)
+    {
+        if (f == null)
+            return;
+
+        try
+        {
+            BufferedReader in = new BufferedReader(new FileReader(f));
+            if (!in.readLine().equals(EXPORT_FIRST_LINE))
+            {
+                JOptionPane.showMessageDialog(null,
+                    "Experience .csv files must begin with the following line:\n"
+                        + EXPORT_FIRST_LINE, "Not Exported File",
+                    JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            String line;
+            while ((line = in.readLine()) != null)
+            {
+                String[] split = line.split(",");
+                int lvl = Integer.parseInt(split[0]);
+                for (int chr = 0; chr < 4; chr++)
+                {
+                    writeExp(chr, lvl, Integer.parseInt(split[chr + 1]), rom);
+                }
+            }
+            in.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            JOptionPane.showMessageDialog(null,
+                "Error: File not saved: File not found.", "Error",
+                JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+            return;
+        }
+        catch (IOException e)
+        {
+            JOptionPane.showMessageDialog(null,
+                "Error: File not saved: Could write file.", "Error",
+                JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+            return;
+        }
+    }
 }
